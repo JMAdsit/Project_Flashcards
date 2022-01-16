26c9fed9a81323aa07e4e34dabc4663c36c9f584
Jeremiah Adsit <jeremiah.adsit@gmail.com>
2022-01-16 14:05:55 -0700

    fleshed out comments in utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,25 @@
 import { 
     listDecks, deleteDeck, deleteCard, updateDeck, 
     createCard, updateCard, createDeck
-} from "../utils/api/index";
+} from "./api/index";
 
-//Handle add deck
+/** Adds a new deck from the form, then reloads the deck list
+ *
+ * useHistory object to move the page to the new deck
+ * @param {*} history 
+ * 
+ * reads deckList to get last deck id
+ * @param {*} deckList 
+ * 
+ * sets deckList after reading from backend
+ * @param {*} setDeckList 
+ * 
+ * deck data from form
+ * @param {*} deck 
+ * 
+ * submit event
+ * @param {*} event 
+ */
 export async function addDeck(history, deckList, setDeckList, deck, event) {
   event.preventDefault();
   console.log(deckList);
@@ -18,7 +34,17 @@ export async function addDeck(history, deckList, setDeckList, deck, event) {
   }
 }
 
-//Handle deck delete
+/** Deletes a deck using its id
+ * 
+ * useHistory object to move back to home
+ * @param {*} history 
+ * 
+ * sets deckList after reading from backend, post-delete
+ * @param {*} setDeckList 
+ * 
+ * id of deck sent to deleteDeck
+ * @param {*} deckId 
+ */
 export async function deckDelete(history, setDeckList, deckId) {
 if(window.confirm("Delete this deck? You will not be able to recover it.")) {
         try {
@@ -32,7 +58,17 @@ if(window.confirm("Delete this deck? You will not be able to recover it.")) {
     }
 }
 
-//Handle edit deck
+/** Edits a deck's info
+ * 
+ * deck data from form
+ * @param {*} updatedDeck
+ * 
+ * sets deckList after reading from backend
+ * @param {*} setDeckList 
+ * 
+ * submit event
+ * @param {*} event 
+ */
 export async function deckEdit(updatedDeck, setDeckList, event) {
   event.preventDefault();
   try {
@@ -44,7 +80,23 @@ export async function deckEdit(updatedDeck, setDeckList, event) {
   }
 }
 
-//Handle add card
+/** Add a card to a deck
+ * 
+ * id of the deck being added to
+ * @param {*} deckId 
+ * 
+ * card data from form
+ * @param {*} card 
+ * 
+ * setCard to change form state back to blank
+ * @param {*} setCard 
+ * 
+ * sets deckList after reading from backend
+ * @param {*} setDeckList 
+ * 
+ * submit event
+ * @param {*} event 
+ */
 export async function addCard(deckId, card, setCard, setDeckList, event) {
   event.preventDefault();
   try {
@@ -57,7 +109,17 @@ export async function addCard(deckId, card, setCard, setDeckList, event) {
   }
 }
 
-//Handle card delete
+/** Deletes a card from a deck
+ * 
+ * useHistory object to reload back after deleting a card
+ * @param {*} history 
+ * 
+ * sets deckList after reading from backend
+ * @param {*} setDeckList 
+ * 
+ * id of card being deleted
+ * @param {*} cardId 
+ */
 export async function cardDelete(history, setDeckList, cardId) {
   if(window.confirm("Delete this card? You will not be able to recover it.")) {
     try {
@@ -71,7 +133,20 @@ export async function cardDelete(history, setDeckList, cardId) {
   }
 }
 
-//Handle edit card
+/** Edit a card in a deck
+ * 
+ * useHistory to move to the deck page
+ * @param {*} history 
+ * 
+ * sets deckList after reading from backend
+ * @param {*} setDeckList 
+ * 
+ * card data from form
+ * @param {*} card 
+ * 
+ * submit event
+ * @param {*} event 
+ */
 export async function editCard(history, setDeckList, card, event) {
   event.preventDefault();
   try {
